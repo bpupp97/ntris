e69df9261a7d014d426d443700e9d8b893252846
Brandon Pupp <bpupp97@gmail.com>
2019-07-05 11:08:30 -0500

    Removed direction definitions from ntris.h

--- ntris.h
+++ ntris.h
@@ -26,20 +26,4 @@ typedef struct _nomino nomino;
 
 #define OUTFILE "ntris.out"
 
-#define N   0x01
-#define E   0x02
-#define NE  0x03
-#define S   0x04
-#define NS  0x05
-#define SE  0x06
-#define NES 0x07
-#define W   0x08
-#define NW  0x09
-#define EW  0x0A
-#define NEW 0x0B
-#define SW  0x0C
-#define NSW 0x0D
-#define SEW 0x0E
-#define NESW    0x0F
-
 #endif // INC_NTRIS_H
